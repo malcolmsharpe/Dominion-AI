315edddb2305b2475962d4e7d6c66b012a90ada7
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2012-02-23 02:47:05 -0500

    Now better supports multiple instances of a strategy.

--- ai.py
+++ ai.py
@@ -152,9 +152,26 @@ class PlayerState(object):
         out.append('%d%s' % (counts[c], c))
     return ', '.join(out)
 
-class PlayerStrategy(object):
-  def __init__(self, idx):
+class Player(object):
+  def __init__(self, idx, strategy):
     self.idx = idx
+    self.strategy = strategy
+
+    self.experimented = True
+    self.prev_features = None
+
+  def update_model(self, game):
+    self.strategy.update_model(self, game)
+
+  def train(self, features=None, outcome=None):
+    self.strategy.train(self, features=features, outcome=outcome)
+
+  def buy(self, game):
+    self.strategy.buy(self, game)
+
+class PlayerStrategy(object):
+  def __init__(self):
+    self.name = self.__class__.__name__
 
   def update_model(self, game):
     pass
@@ -162,25 +179,31 @@ class PlayerStrategy(object):
   def train(self, features=None, outcome=None):
     pass
 
+  def create_player(self, idx):
+    return Player(idx, self)
+
+  def buy(self, player, game):
+    pass
+
 FEATURE_COUNT = 4
 
 class LearningPlayerStrategy(PlayerStrategy):
-  def __init__(self, idx):
-    PlayerStrategy.__init__(self, idx)
-    self.experimented = True
-    self.prev_features = None
+  def __init__(self):
+    PlayerStrategy.__init__(self)
+    self.compressed_A = numpy.array([[0.0]*FEATURE_COUNT for i in range(FEATURE_COUNT)])
+    self.compressed_b = numpy.array([0.0]*FEATURE_COUNT)
 
-  def update_model(self, game):
+  def update_model(self, player, game):
     # Update model.
-    features = self.extract_features(game)
-    self.train(features=features)
-    self.prev_features = features
+    features = self.extract_features(player, game)
+    self.train(player, features=features)
+    player.prev_features = features
 
   def evaluate_features(self, features):
-    return self.__class__.weights.dot(features)
+    return self.weights.dot(features)
 
-  def evaluate(self, game):
-    return self.evaluate_features(self.extract_features(game))
+  def evaluate(self, player, game):
+    return self.evaluate_features(self.extract_features(player, game))
 
   def player_features(self, game, idx):
     ret = []
@@ -196,30 +219,30 @@ class LearningPlayerStrategy(PlayerStrategy):
 
     return ret
 
-  def extract_features(self, game):
-    lst = self.player_features(game, self.idx) + self.player_features(game, 1-self.idx)
+  def extract_features(self, player, game):
+    lst = self.player_features(game, player.idx) + self.player_features(game, 1-player.idx)
     return numpy.array(lst)
 
-  def train(self, features=None, outcome=None):
+  def train(self, player, features=None, outcome=None):
     global sumsqerr
     global nsamples
 
     assert features is None or outcome is None
     assert features is not None or outcome is not None
 
-    if self.prev_features is None:
-      say('  Player %d skipping model training on first turn.' % self.idx)
+    if player.prev_features is None:
+      say('  Player %d skipping model training on first turn.' % player.idx)
       return
-    if self.experimented:
-      say('  Player %d skipping model training due to experiment.' % self.idx)
+    if player.experimented:
+      say('  Player %d skipping model training due to experiment.' % player.idx)
       return
 
     if features is not None: mode = 'difference'
     else: mode = 'final'
-    say('  Player %d training %s:' % (self.idx, mode))
+    say('  Player %d training %s:' % (player.idx, mode))
 
-    x_t = self.prev_features
-    w = self.__class__.weights
+    x_t = player.prev_features
+    w = self.weights
     w_x_t = w.dot(x_t)
 
     say('    w = %s' % w)
@@ -238,15 +261,15 @@ class LearningPlayerStrategy(PlayerStrategy):
       x_t_m = numpy.matrix(x_t).transpose()
       x_t1_m = numpy.matrix(x_t1).transpose()
 
-      self.__class__.compressed_A += x_t_m * (x_t_m - x_t1_m).transpose()
+      self.compressed_A += x_t_m * (x_t_m - x_t1_m).transpose()
     else:
       say('    outcome = %.8lf' % outcome)
       err = outcome - w_x_t
 
       x_t_m = numpy.matrix(x_t).transpose()
 
-      self.__class__.compressed_A += x_t_m * x_t_m.transpose()
-      self.__class__.compressed_b += outcome * x_t
+      self.compressed_A += x_t_m * x_t_m.transpose()
+      self.compressed_b += outcome * x_t
 
     say('    err = %.8lf' % err)
 
@@ -256,27 +279,26 @@ class LearningPlayerStrategy(PlayerStrategy):
     adjustment = -alpha * err * gradient
     say('    adjustment = %s' % adjustment)
 
-    self.__class__.weights = w + adjustment
+    self.weights = w + adjustment
 
-    say('    w\' = %s' % self.__class__.weights)
+    say('    w\' = %s' % self.weights)
     say('')
 
 class BasicBigMoney(PlayerStrategy):
-  def buy(self, game):
-    ps = game.states[self.idx]
+  def buy(self, player, game):
+    ps = game.states[player.idx]
     ps.buy('p')
     ps.buy('g')
     ps.buy('s')
 
 class BigMoneyUltimate(LearningPlayerStrategy):
-  compressed_A = numpy.array([[0.0]*FEATURE_COUNT for i in range(FEATURE_COUNT)])
-  compressed_b = numpy.array([0.0]*FEATURE_COUNT)
+  def __init__(self):
+    LearningPlayerStrategy.__init__(self)
+    #self.weights = numpy.array([1.0,0.6,-1.0,-0.6])
+    self.weights = numpy.array([0.0,1.0,-0.0,-1.0])
 
-  #weights = numpy.array([1.0,0.6,-1.0,-0.6])
-  weights = numpy.array([0.0,1.0,-0.0,-1.0])
-
-  def buy(self, game):
-    ps = game.states[self.idx]
+  def buy(self, player, game):
+    ps = game.states[player.idx]
     if ps.total_money_in_deck() > 18:
       ps.buy('p')
     if ps.game.supply['p'] <= 4:
@@ -294,42 +316,38 @@ sumsqerr = 0.0
 nsamples = 0
 msqerrf = file('msqerr.txt', 'w')
 
-def show_learn_data(cls):
+def show_learn_data(strategy):
   global sumsqerr
   global nsamples
   msqerr = sumsqerr / nsamples
   sumsqerr = 0.0
   nsamples = 0
-  print '==> %s weights: %s (msq err = %.8lf)' % (cls.__name__, cls.weights, msqerr)
+  print '==> %s weights: %s (msq err = %.8lf)' % (strategy.name, strategy.weights, msqerr)
   print >>msqerrf, msqerr
 
 class SimpleAI(LearningPlayerStrategy):
-  compressed_A = numpy.array([[0.0]*FEATURE_COUNT for i in range(FEATURE_COUNT)])
-  compressed_b = numpy.array([0.0]*FEATURE_COUNT)
+  def __init__(self):
+    LearningPlayerStrategy.__init__(self)
+    #self.weights = numpy.array([random.gauss(0,1) for i in range(2)])
+    #self.weights = numpy.array([1.0,0.6,-1.0,-0.6])
+    #self.weights = numpy.array([1.0,1.0,-1.0,-1.0])
 
-  #weights = numpy.array([random.gauss(0,1) for i in range(2)])
-  #weights = numpy.array([1.0,0.6,-1.0,-0.6])
-  #weights = numpy.array([1.0,1.0,-1.0,-1.0])
+    # These weights were obtained from 1000 self-plays.
+    #self.weights = numpy.array([ 0.95707939,  1.93092173, -1.02307045, -1.93986092])
 
-  # These weights were obtained from 1000 self-plays.
-  #weights = numpy.array([ 0.95707939,  1.93092173, -1.02307045, -1.93986092])
+    # These weights were obtained by playing BMU against itself for 10,000 plays,
+    # then applying the LSTD algorithm to compute weights for which the sum of TD
+    # updates is zero.
+    # These weights make for a high-quality AI.
+    self.weights = numpy.array([ 4.80675988, 3.00011302, -5.08273628, -2.92180695])
 
-  # These weights were obtained by playing BMU against itself for 10,000 plays,
-  # then applying the LSTD algorithm to compute weights for which the sum of TD
-  # updates is zero.
-  # These weights make for a high-quality AI.
-  weights = numpy.array([ 4.80675988, 3.00011302, -5.08273628, -2.92180695])
+    # These weights were obtained by playing SimpleAI against itself for 1000 plays,
+    # starting with the previous weights and updating incrementally,
+    # then applying the LSTD algorithm to compute weights.
+    #self.weights = numpy.array([ 6.00857446,  3.15251864, -6.38471951, -3.00783411])
 
-  # These weights were obtained by playing SimpleAI against itself for 1000 plays,
-  # starting with the previous weights and updating incrementally,
-  # then applying the LSTD algorithm to compute weights.
-  #weights = numpy.array([ 6.00857446,  3.15251864, -6.38471951, -3.00783411])
-
-  def __init__(self, idx):
-    LearningPlayerStrategy.__init__(self, idx)
-
-  def buy(self, game):
-    ps = game.states[self.idx]
+  def buy(self, player, game):
+    ps = game.states[player.idx]
     buy_options = ['']
     for c in costs:
       if costs[c] <= ps.money and game.supply[c] > 0:
@@ -340,25 +358,25 @@ class SimpleAI(LearningPlayerStrategy):
       bestc = 'x'
       for c in buy_options:
         future = game.copy(True)
-        ps = future.states[self.idx]
+        ps = future.states[player.idx]
         if c:
           ps.buy(c)
         ps.cleanup()
-        now = self.evaluate(future)
+        now = self.evaluate(player, future)
         say('  Buying "%s" gives me value %.8lf' % (c,now))
         if now > best:
           best = now
           bestc = c
       assert bestc != 'x'
-      self.experimented = False
+      player.experimented = False
     else:
       # Don't propagate results of experiments backwards!
-      self.experimented = True
+      player.experimented = True
       bestc = random.choice(buy_options)
       say('  Experimenting.')
 
     if bestc:
-      ps = game.states[self.idx]
+      ps = game.states[player.idx]
       ps.buy(bestc)
 
 TURN_LIMIT = 50
@@ -366,19 +384,19 @@ class Game(object):
   def __init__(self, parent=None, silent=False):
     self.silent = silent
     if parent:
-      self.strategy_types = parent.strategy_types
+      self.strategies = parent.strategies
       self.states = [ps.copy(self, silent) for ps in parent.states]
-      self.strategies = [s for s in parent.strategies]
+      self.players = [p for p in parent.players]
       self.supply = copy.copy(parent.supply)
     else:
-      self.strategy_types = STRATEGY_TYPES
-      random.shuffle(self.strategy_types)
+      self.strategies = STRATEGIES
+      random.shuffle(self.strategies)
 
       self.states = []
-      self.strategies = []
-      for i,t in enumerate(self.strategy_types):
-        self.states.append(PlayerState(self, '%s (player %d)' % (t.__name__, i+1)))
-        self.strategies.append(t(i))
+      self.players = []
+      for i,s in enumerate(self.strategies):
+        self.states.append(PlayerState(self, '%s (player %d)' % (s.name, i+1)))
+        self.players.append(s.create_player(i))
 
       self.supply = {
         'c': 60,
@@ -406,18 +424,18 @@ class Game(object):
 
     done = False
     for turn in range(TURN_LIMIT):
-      for ps,strat in zip(self.states, self.strategies):
+      for ps,player in zip(self.states, self.players):
         self.say('')
         self.say('*** %s\'s turn %d' % (ps.name, turn))
 
         while ps.play('c'): pass
         while ps.play('s'): pass
         while ps.play('g'): pass
-        strat.buy(self)
+        player.buy(self)
 
         # Clean up hand and draw a new hand.
         ps.cleanup()
-        strat.update_model(self)
+        player.update_model(self)
         ps.draw(5)
 
         self.say('Provinces remaining in supply: %d' % self.supply['p'])
@@ -440,29 +458,35 @@ class Game(object):
     self.say('*** GAME OVER')
     for ps in self.states:
       self.say('%s had %d points: %s.' % (ps.name, ps.get_vp(), ps.format_deck()))
-    p1,p2 = self.states
-    s1,s2 = self.strategies
-    if p1.get_vp() > p2.get_vp():
-      self.say('%s wins!' % p1.name)
-      s1.train(outcome=1)
-      s2.train(outcome=-1)
-      return [p1.name]
-    elif p2.get_vp() > p1.get_vp() or (p2.get_vp() == p1.get_vp() and p2.turns < p1.turns):
-      self.say('%s wins!' % p2.name)
-      s1.train(outcome=-1)
-      s2.train(outcome=1)
-      return [p2.name]
+    st1,st2 = self.states
+    p1,p2 = self.players
+    if st1.get_vp() > st2.get_vp():
+      self.say('%s wins!' % st1.name)
+      p1.train(outcome=1)
+      p2.train(outcome=-1)
+      return [st1.name]
+    elif st2.get_vp() > st1.get_vp() or (st2.get_vp() == st1.get_vp() and st2.turns < st1.turns):
+      self.say('%s wins!' % st2.name)
+      p1.train(outcome=-1)
+      p2.train(outcome=1)
+      return [st2.name]
     else:
-      self.say('%s and %s rejoice in their shared victory!' % (p1.name, p2.name))
-      s1.train(outcome=0)
-      s2.train(outcome=0)
-      return [p1.name, p2.name]
+      self.say('%s and %s rejoice in their shared victory!' % (st1.name, st2.name))
+      p1.train(outcome=0)
+      p2.train(outcome=0)
+      return [st1.name, st2.name]
 
 alpha = 0.0
 experiment_p = 0.0
 
-klass = SimpleAI
-STRATEGY_TYPES = [SimpleAI, SimpleAI]
+strategy = SimpleAI()
+
+# Weights found using SimpleAI self-play using BMU weights, without
+# incremental updates.
+strategy.name = 'SimpleAI smart?'
+strategy.weights = numpy.array([ 5.76272973, 3.0919074, -6.12896186, -2.94875218])
+
+STRATEGIES = [strategy, SimpleAI()]
 
 def main():
   global alpha
@@ -478,7 +502,7 @@ def main():
     game = Game()
     winners = game.play()
     print 'Game %d' % i,
-    show_learn_data(klass)
+    show_learn_data(strategy)
     print '  Winner %s' % winners
     winners = tuple(winners)
     wins[winners] = wins.get(winners,0) + 1
@@ -487,6 +511,6 @@ def main():
     print '%3d: %s' % (n,w)
 
   f = file('lsq_data.txt', 'w')
-  print >>f, repr((klass.compressed_A, klass.compressed_b, klass.weights))
+  print >>f, repr((strategy.compressed_A, strategy.compressed_b, strategy.weights))
 
 main()
